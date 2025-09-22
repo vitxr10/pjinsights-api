bd7f8edd59bea3f7fa5032754a36a473f4d883e8
Carlos Henrique Lins Ponchirolli <carlosponchirolli655@gmail.com>
2025-09-21 22:07:08 -0300

    refactor: alterado o RegisterRequest

--- src/main/java/br/com/santander/pjinsight/application/model/request/RegisterRequest.java
+++ src/main/java/br/com/santander/pjinsight/application/model/request/RegisterRequest.java
@@ -9,9 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
-
     private String login;
     private String password;
-    private UserRoleEnum role;
-
 }
